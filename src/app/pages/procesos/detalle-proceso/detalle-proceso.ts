6623a92f6d8592bfe1a053401969ceaa14f87f67
SarLua <sara.ocampo2520@gmail.com>
2026-04-26 21:59:57 -0500

    Seleccionar un proceso

---
+++ src/app/pages/procesos/detalle-proceso/detalle-proceso.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-proceso',
+  imports: [],
+  templateUrl: './detalle-proceso.html',
+  styleUrl: './detalle-proceso.css',
+})
+export class DetalleProceso {}
---
+++ src/app/pages/procesos/editor-proceso/editor-proceso.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-proceso',
+  imports: [],
+  templateUrl: './editor-proceso.html',
+  styleUrl: './editor-proceso.css',
+})
+export class EditorProceso {}

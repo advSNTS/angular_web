8d05abb048f94640e74f05e05ac2f8a1042430c4
DeadlyMochi <diegofernandocastrilloncortes@gmail.com>
2026-05-05 16:41:49 -0500

    Update api-url.ts

--- src/app/core/api-url.ts
+++ src/app/core/api-url.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://grupo11.inphotech.co/api';
+const API_BASE_URL = 'https://grupo11.inphotech.co/api';
 
 export function buildApiPath(path: string): string {
   const normalizedPath = path.startsWith('/') ? path : `/${path}`;

d5d64972637e6fad085df8164f66eb350a3862cc
DeadlyMochi <diegofernandocastrilloncortes@gmail.com>
2026-05-12 15:11:19 -0500

    Change apiUrl from HTTPS to HTTP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  /** En desarrollo las peticiones van a `/api/*` (proxy → backend). */
-  apiUrl: 'https://grupo11.inphotech.co/api'
+  apiUrl: 'http://grupo11.inphotech.co/api'
 };

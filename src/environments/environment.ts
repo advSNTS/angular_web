042cd281b12ad6aa27052c81efa63b3d034b829b
Santiago Jose hernandez <www.santiagohernandez@gmail.com>
2026-05-12 15:08:41 -0500

    ip profesor
    cambio ip profesor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   /** En desarrollo las peticiones van a `/api/*` (proxy → backend). */
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://grupo11.inphotech.co/api'
 };

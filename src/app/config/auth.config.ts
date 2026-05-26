87bbc6c6f729ae3ec3a85009916d5c924ac189bb
PainterB33 <thomasarevalo123@gmail.com>
2026-05-26 17:40:34 -0500

    Si

--- src/app/config/auth.config.ts
+++ src/app/config/auth.config.ts
@@ -1,4 +1,4 @@
 import { buildApiPath } from '../core/api-url';
 
-/** URL completa del endpoint de login (incluye prefijo `/api` en desarrollo). */
+/** URL completa del endpoint de login (incluye prefijo `/api` en desarrollo). m*/
 export const AUTH_LOGIN_URL = buildApiPath('/empleados/login');
